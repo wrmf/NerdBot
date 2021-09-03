5ac2f946dab6ab94cc289ca6d500e2cde443c1bb
wrmf <31448920+wrmf@users.noreply.github.com>
2021-09-03 15:54:53 -0500

    ??

--- cogs/giveaway.py
+++ cogs/giveaway.py
@@ -37,15 +37,6 @@ def is_mod(ctx: commands.Context):
     roles: List[discord.Role] = member.roles
     return is_admin(ctx) or any([role.permissions.manage_messages for role in roles])
 
-def is_LDL_channel(ctx: commands.Context):
-    if(ctx.guild.id == LDL_server):
-        if(ctx.message.channel.id == LDL_bot_commands or ctx.message.channel.id == LDL_bot_test):
-            return True
-        else:
-            return False
-    else:
-        return True
-
 class Giveaway(commands.Cog):
     """
     Giveaway commands
@@ -60,15 +51,15 @@ class Giveaway(commands.Cog):
         """
         Create a giveaway
         """
-        if (is_LDL_channel(ctx)):
-            #reaction = '🎉'
-            reaction = '<:celebrate_animated:883452218840334346>'
+        
+        #reaction = '🎉'
+        reaction = '<:celebrate_animated:883452218840334346>'
 
-            embed = discord.Embed(title="Giveaway!", description=item)
-            react_message = await ctx.send(embed=embed)
-            await react_message.add_reaction(reaction)
-            embed.set_footer(text='Giveaway ID: {}'.format(react_message.id))
-            await react_message.edit(embed=embed)
+        embed = discord.Embed(title="Giveaway!", description=item)
+        react_message = await ctx.send(embed=embed)
+        await react_message.add_reaction(reaction)
+        embed.set_footer(text='Giveaway ID: {}'.format(react_message.id))
+        await react_message.edit(embed=embed)
 
 
     @commands.command(pass_context=True)
