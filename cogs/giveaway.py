74f243955412c4c84996ee10221717a5aac22e4e
wrmf <31448920+wrmf@users.noreply.github.com>
2021-09-03 15:51:15 -0500

    __innit__

--- cogs/giveaway.py
+++ cogs/giveaway.py
@@ -46,6 +46,14 @@ def is_LDL_channel(ctx: commands.Context):
     else:
         return True
 
+class Giveaway(commands.Cog):
+    """
+    Giveaway commands
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
 @commands.command(pass_context=True)
 @commands.check(is_admin)
 async def giveaway(self, ctx: commands.Context, item: str):
@@ -63,26 +71,26 @@ async def giveaway(self, ctx: commands.Context, item: str):
         await react_message.edit(embed=embed)
 
 
-@commands.command(pass_context=True)
-async def roll(self, ctx: commands.Context, id):
-    giveaway_message = await ctx.fetch_message(id)
-    if not giveaway_message.embeds:
-        return
-    voters = [ctx.me.id]  # add the bot's ID to the list of voters to exclude it's votes
+    @commands.command(pass_context=True)
+    async def roll(self, ctx: commands.Context, id):
+        giveaway_message = await ctx.fetch_message(id)
+        if not giveaway_message.embeds:
+            return
+        voters = [ctx.me.id]  # add the bot's ID to the list of voters to exclude it's votes
 
-    for reaction in giveaway_message.reactions:
-        reactors = await reaction.users().flatten()
-        for reactor in reactors:
-            if reactor.id not in voters:
-                voters.append(reactor.id)
-    voters.remove(giveaway_message.author.id)
-    if (len(voters) <= 0):
-        embed = discord.Embed(title="ERROR", description="No one has entered this giveaway!",
-                              color=ctx.message.author.top_role.color)
-        await ctx.send(embed=embed)
-        return
-    winner_id = str(voters[random.randint(0, len(voters) - 1)])
-    await ctx.send("Congrats on winning <@" + winner_id + ">!")
-    embed = discord.Embed(title="Giveaway!", description="Winner: <@" + winner_id + ">!")
-    embed.set_footer(text='Giveaway ID: {}'.format(giveaway_message.id))
-    await giveaway_message.edit(embed=embed)
+        for reaction in giveaway_message.reactions:
+            reactors = await reaction.users().flatten()
+            for reactor in reactors:
+                if reactor.id not in voters:
+                    voters.append(reactor.id)
+        voters.remove(giveaway_message.author.id)
+        if (len(voters) <= 0):
+            embed = discord.Embed(title="ERROR", description="No one has entered this giveaway!",
+                                  color=ctx.message.author.top_role.color)
+            await ctx.send(embed=embed)
+            return
+        winner_id = str(voters[random.randint(0, len(voters) - 1)])
+        await ctx.send("Congrats on winning <@" + winner_id + ">!")
+        embed = discord.Embed(title="Giveaway!", description="Winner: <@" + winner_id + ">!")
+        embed.set_footer(text='Giveaway ID: {}'.format(giveaway_message.id))
+        await giveaway_message.edit(embed=embed)
