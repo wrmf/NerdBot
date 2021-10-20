b443ceb956798f874099dfd4e3c75a1f64263359
wrmf <31448920+wrmf@users.noreply.github.com>
2021-10-19 22:33:29 -0500

    testing trivia start

--- cogs/trivia.py
+++ cogs/trivia.py
@@ -8,8 +8,8 @@ import discord
 from discord.ext import commands
 from bot import *
 import random
-from permissions import *
-from triviaCategoriesList import *
+from NerdBot.permissions import *
+from NerdBot.triviaCategoriesList import triviaCategoriesList
 
 
 class Trivia(commands.Cog):
@@ -21,7 +21,7 @@ class Trivia(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=['triviacategories'])
-    async def triviaCategories(self, ctx, status: str = None):
+    async def triviaCategories(self, ctx):
 
         embed = discord.Embed(title="Trivia", description=f"The available trivia categories are:",
                               color=ctx.message.author.top_role.color) #Create embed
@@ -30,6 +30,16 @@ class Trivia(commands.Cog):
             embed.add_field(name=i, value=category, inline=True) #Set title for first embed
             i+=i
         await ctx.send(embed=embed) #Send embed
+
+    @commands.command(aliases=['triviastart'])
+    async def triviaStart(self, ctx, category: str):
+
+        if(category is None or category not in triviaCategoriesList):
+            embed = discord.Embed(title="ERROR", description=f"Category {category} is not a valid category. Please do ~triviaCategories for the full list of categories",
+                                color=ctx.message.author.top_role.color)  # Create embed
+            await ctx.send(embed=embed)  # Send embed
+        else:
+            await ctx.send("idk I haven't gotten this far in my coding yet leave me alone")
 
 def setup(bot):
     bot.add_cog(Trivia(bot))
