503190d771830ad1c23cf9709dc43fcde94d8501
wrmf <31448920+wrmf@users.noreply.github.com>
2021-10-20 14:08:26 -0500

    better?

--- cogs/trivia.py
+++ cogs/trivia.py
@@ -94,11 +94,11 @@ async def airportCodesTrivia(self, ctx, questions):
             await ctx.send(f"Placement of right answer is {placementOfRightAnswer}")
             counterWrongAnswer = 0
 
-            for counter2 in range (1, 4):
+            for counter2 in range (1, 5):
                 if counter2 == placementOfRightAnswer:
                     embed.add_field(name=counter2, value=airportCodesList[1][num], inline=True)  #Get right answer added
                 else:
-                    embed.add_field(name=counter2, value=listOfAnswers[counterWrongAnswer], inline=True)  # Set title for first embed
+                    embed.add_field(name=counter2, value=airportCodesList[1][listOfAnswers[counterWrongAnswer]], inline=True)  # Set title for first embed
                     counterWrongAnswer+=1
             await ctx.send(embed=embed)
 
