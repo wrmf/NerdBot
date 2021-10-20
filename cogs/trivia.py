68a8ddcb63ce436468f392993232f9f70dc68216
wrmf <31448920+wrmf@users.noreply.github.com>
2021-10-20 09:19:16 -0500

    error message for too many messages

--- cogs/trivia.py
+++ cogs/trivia.py
@@ -12,6 +12,9 @@ from permissions import *
 from triviaCategoriesList import triviaCategoriesList
 
 
+maxTriviaQuestions = 10
+
+
 class Trivia(commands.Cog):
     """
     Trivia related commands
@@ -33,6 +36,7 @@ class Trivia(commands.Cog):
 
     @commands.command(aliases=['triviastart'])
     async def triviaStart(self, ctx, category: str = None):
+        msg = 0
 
         if(category is None or category not in triviaCategoriesList):
             embed = discord.Embed(title="ERROR", description=f"Category **{category}** is not a valid category. Please do ~triviaCategories for the full list of categories",
@@ -40,17 +44,22 @@ class Trivia(commands.Cog):
             await ctx.send(embed=embed)  # Send embed
         else:
             embed = discord.Embed(title="Trivia",
-                                  description=f"How many questions would you like? You can have up to 10 questions",
+                                  description=f"How many questions would you like? You can have up to {maxTriviaQuestions} questions",
                                   color=ctx.message.author.top_role.color)  # Create embed
             await ctx.send(embed=embed)  # Send embed
             try:
                 msg = await client.wait_for('messsage', timeout=15.0)
+                if msg < 1 or msg > maxTriviaQuestions:
+                    embed = discord.Embed(title="ERROR",
+                                          description=f"Having a trivia game with {msg} number of questions is not valid at the moment. Please try again and enter a number between 1 and 10.",
+                                          color=ctx.message.author.top_role.color)  # Create embed
+                    await ctx.send(embed=embed)  # Send embed
             except asyncio.TimeoutError:
-                embed = discord.Embed(title="ERROR",
-                                      description=f"",
+                embed = discord.Embed(title="ERROR", description=f"Trivia Start timed out. Please try again and send a message quicker next time!",
                                       color=ctx.message.author.top_role.color)  # Create embed
                 await ctx.send(embed=embed)  # Send embed
                 return
+
 
 def setup(bot):
     bot.add_cog(Trivia(bot))
