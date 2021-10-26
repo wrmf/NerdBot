fdf3e36ae1ef0e81840f8f13f6149c37b98320e8
wrmf <31448920+wrmf@users.noreply.github.com>
2021-10-25 22:56:20 -0500

    is it isNumber?

--- cogs/trivia.py
+++ cogs/trivia.py
@@ -119,7 +119,7 @@ async def startTrivia(self, ctx, questions, originalChannel, question, questionL
         await ctx.send(embed=embed) #Send embed
 
         def checkCustom(message: discord.Message): #Check for message
-            if message.isInteger():
+            if message.isNumber():
                 if message.channel == originalChannel and int(message.content) == placementOfRightAnswer+1 and message.author.id not in thisQuestionAnswers:
                     return True
                 else:
