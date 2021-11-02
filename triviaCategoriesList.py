7b59bc7c051187fc5145ab85612f9b9238d6a819
wrmf <31448920+wrmf@users.noreply.github.com>
2021-11-02 11:51:31 -0500

    square bracket was off

--- triviaCategoriesList.py
+++ triviaCategoriesList.py
@@ -2,4 +2,4 @@ from airportCodes import airportCodesList
 from trivia.triviaElements import triviaElementsList
 
 
-triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesList[0], airportCodesList[1]], triviaElementsList[0]]
+triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesList[0], airportCodesList[1], triviaElementsList[0]]]
