fa018397b9967c710569268d84cef332a98facd6
wrmf <31448920+wrmf@users.noreply.github.com>
2021-11-02 10:53:53 -0500

    pathing error (fixed)

--- triviaCategoriesList.py
+++ triviaCategoriesList.py
@@ -1,5 +1,5 @@
-from NerdBot.trivia.airportCodes import airportCodesList
-from NerdBot.trivia.triviaElements import triviaElementsList
+from trivia.airportCodes import airportCodesList
+from trivia.triviaElements import triviaElementsList
 
 
 triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesList[0], airportCodesList[1]], triviaElementsList[0]]
