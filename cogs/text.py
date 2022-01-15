fc108d9559e2946e5be8d6abcda652d4f62efa34
wrmf <31448920+wrmf@users.noreply.github.com>
2022-01-15 16:03:10 -0700

    Update text.py

--- cogs/text.py
+++ cogs/text.py
@@ -44,5 +44,33 @@ class Text(commands.Cog):
                 value=f"LOL that's not a valid command")
             await channel.send(embed=embed)
 
+    @commands.command()
+    @commands.check(is_admin)
+    async def dm(self, ctx: commands.Context, id: int, message: str, num: int = 1):
+        """
+        Direct message a user a message
+        @author Nerd#2022
+        """
+
+        if ctx.message.author.id == 555207100603826177:
+            i = 0
+
+            await ctx.channel.purge(limit=1)  # Delete original message
+
+            while i < num:
+                user = await ctx.guild.fetch_member(id)
+                user.send(embed=embed)
+                await user.send(message) #Send message
+                i+=1
+
+        else:
+            embed = discord.Embed(color=ctx.message.author.top_role.color.value)
+            embed.add_field(
+                name=f"ERROR",
+                value=f"LOL that's not a valid command")
+            await channel.send(embed=embed)
+
+
+
 def setup(bot):
     bot.add_cog(Text(bot))
