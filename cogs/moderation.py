260b8540e2486f976fb0d38accde122e0798aed8
wrmf <31448920+wrmf@users.noreply.github.com>
2022-03-27 18:38:15 -0500

    removing reason rq

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -109,7 +109,7 @@ class Moderation(commands.Cog):
                     await ctx.send(embed=embed)  # Say in chat
                 else:
                     try:
-                        await ctx.guild.ban(member, reason=reason)
+                        await ctx.guild.ban(member)#, reason=reason)
                         if embedCounter > 24:
                             embedCounter = 0
                             await ctx.send(embed=banEmbed)
