2664d994566aeddc58fb64d0fac0f892e222c815
wrmf <31448920+wrmf@users.noreply.github.com>
2022-03-30 23:18:35 -0400

    fixing import?

--- triviaCategoriesList.py
+++ triviaCategoriesList.py
@@ -1,4 +1,4 @@
 from trivia.triviaElements import triviaElementsList
-from NerdBot.bot import airportCodesTriviaDataframe
+from bot import airportCodesTriviaDataframe
 
 triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesTriviaDataframe["City"], airportCodesTriviaDataframe["Code"], triviaElementsList[0]]]
