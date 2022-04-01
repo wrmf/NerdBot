37a6cdc7977d0176ca3ee343d9a41a15a963e920
wrmf <31448920+wrmf@users.noreply.github.com>
2022-03-31 21:11:08 -0400

    e

--- triviaCategoriesList.py
+++ triviaCategoriesList.py
@@ -11,7 +11,7 @@ elementsDataframe.sort_values("Number")  # Sort values by code... does this do a
 
 # Read in CSV for airport codes trivia
 columnsAirportCodes = ["Code", "Name", "Random"]
-airportCodesDataframe = pd.read_csv("trivia/airportCodes.csv", header = None, delimiter='"', names=columnsAirportCodes)
+airportCodesDataframe = pd.read_csv("trivia/airportCodesText.csv", header = None, delimiter='"', names=columnsAirportCodes)
 airportCodesDataframe.sort_values("Code") #Sort values by code... does this do anything?
 
-triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesList[0], airportCodesList[1], elementsDataframe["Name"]]]
+triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesDataframe["Name"], airportCodesDataframe["Code"], elementsDataframe["Name"]]]
