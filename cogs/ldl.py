e19ccf5dbe537840655c53dae2293ee38e7614e5
wrmf <31448920+wrmf@users.noreply.github.com>
2022-04-01 19:10:32 -0400

    Random

--- cogs/ldl.py
+++ cogs/ldl.py
@@ -160,7 +160,7 @@ class ldl(commands.Cog):
             endDate = endDateStr.split("/")
             ldlLOADataframe = bot.getLOA()
 
-            columns = ["ID", "startDay", "startMonth", "startYear", "endDay", "endMonth", "endYear"]  # Columns for pandas array
+            columns = ["Random", "ID", "startDay", "startMonth", "startYear", "endDay", "endMonth", "endYear"]  # Columns for pandas array
             tempDataframe = pd.DataFrame({"ID":[ctx.message.author.id], "startDay":[startDate[0]],
                                             "startMonth":[startDate[1]], "startYear":[startDate[2]],
                                             "endDay":[endDate[0]], "endMonth":[endDate[1]], "endYear":[endDate[2]]})
