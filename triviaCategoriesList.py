cc4fe278c931981462f8761260b1d5f9ecad20f2
wrmf <31448920+wrmf@users.noreply.github.com>
2022-03-31 21:16:35 -0400

    going back to old system, new system was too intensive on CPU

--- triviaCategoriesList.py
+++ triviaCategoriesList.py
@@ -9,9 +9,9 @@ elementsDataframe = pd.read_csv("trivia/triviaElementsText.csv", header=None, de
 elementsDataframe["Number"] = elementsDataframe["Number"].str[:-1]  # Delete ) from end of string
 elementsDataframe.sort_values("Number")  # Sort values by code... does this do anything?
 
-# Read in CSV for airport codes trivia
-columnsAirportCodes = ["Code", "Name", "Random"]
-airportCodesDataframe = pd.read_csv("trivia/airportCodesText.csv", header = None, delimiter='"', names=columnsAirportCodes)
-airportCodesDataframe.sort_values("Code") #Sort values by code... does this do anything?
+# # Read in CSV for airport codes trivia
+# columnsAirportCodes = ["Code", "Name", "Random"]
+# airportCodesDataframe = pd.read_csv("trivia/airportCodesText.csv", header = None, delimiter='"', names=columnsAirportCodes)
+# airportCodesDataframe.sort_values("Code") #Sort values by code... does this do anything?
 
-triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesDataframe["Name"], airportCodesDataframe["Code"], elementsDataframe["Name"]]]
+triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesList[0], airportCodesList[1], elementsDataframe["Name"]]]
