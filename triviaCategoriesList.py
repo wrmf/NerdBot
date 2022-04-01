571740023ffa8b2dfce249404831842392d4d17c
wrmf <31448920+wrmf@users.noreply.github.com>
2022-03-31 20:59:41 -0400

    import?

--- triviaCategoriesList.py
+++ triviaCategoriesList.py
@@ -1,19 +1,17 @@
 from trivia.triviaElements import triviaElementsList
 from trivia.airportCodes import airportCodesList
 import pandas as pd
-import trivia.triviaElements
+import trivia.triviaElementsText
+import trivia.airportCodesText
 
-# # Read in CSV for airport code trivia
-# columns = ["City", "Code"]  # Columns for pandas array
-# fileNames = ["airportCodes.csv"]
-# airportCodesTriviaDataframe = pd.read_csv(fileNames, header=None, delimiter="(", names=columns)
-# airportCodesTriviaDataframe["Code"] = airportCodesTriviaDataframe["Code"].str[:-1]  # Delete ) from end of string
-# airportCodesTriviaDataframe.sort_values("Code")  # Sort values by code... does this do anything?
-
-# Read in CSV for airport code trivia
+# Read in CSV for elements trivia
 columns = ["Name", "Number"]  # Columns for pandas array
 elementsDataframe = pd.read_csv("trivia/triviaElementsText.csv", header=None, delimiter="(", names=columns)
 elementsDataframe["Number"] = elementsDataframe["Number"].str[:-1]  # Delete ) from end of string
 elementsDataframe.sort_values("Number")  # Sort values by code... does this do anything?
+
+# Read in CSV for airport codes trivia
+airportCodesDataframe = pd.read_csv("", header = None, delimiter='"', names=columns)
+airportCodesDataframe.sort_values("Code") #Sort values by code... does this do anything?
 
 triviaCategoriesList = [["airport codes", "airport names", "elements"], [airportCodesList[0], airportCodesList[1], elementsDataframe["Name"]]]
